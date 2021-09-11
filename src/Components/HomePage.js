99417ec5225d4399e40508f364c81f6ba0acee74
Anisha Gupta <anisha8057@gmail.com>
2021-09-11 18:22:23 +0530

    first commit

---
+++ src/Components/HomePage.js
@@ -0,0 +1,110 @@
+import React from "react";
+import { Row, Form,Select,Spin} from "antd";
+// import { Select,  } from 'antd';
+import debounce from 'lodash/debounce';
+
+const {Option} = Select;
+
+const HomePage = () => {
+  const [form] = Form.useForm();
+  const [value, setValue] = React.useState([]);
+  const formLayout = {
+    labelCol: {
+      span: 4,
+    },
+    wrapperCol: {
+      span: 14,
+    },
+  };
+  const children = [];
+  for (let i = 10; i < 36; i++) {
+    children.push(
+      <Option key={i.toString(36) + i}>{i.toString(36) + i}</Option>
+    );
+  }
+
+  function handleChange(value) {
+    console.log(`selected ${value}`);
+  }
+  function DebounceSelect({ fetchOptions, debounceTimeout = 800, ...props }) {
+    const [fetching, setFetching] = React.useState(false);
+    const [options, setOptions] = React.useState([]);
+    
+    const fetchRef = React.useRef(0);
+    const debounceFetcher = React.useMemo(() => {
+      const loadOptions = (value) => {
+        fetchRef.current += 1;
+        const fetchId = fetchRef.current;
+        setOptions([]);
+        setFetching(true);
+        fetchOptions(value).then((newOptions) => {
+          if (fetchId !== fetchRef.current) {
+            // for fetch callback order
+            return;
+          }
+  
+          setOptions(newOptions);
+          setFetching(false);
+        });
+      };
+  
+      return debounce(loadOptions, debounceTimeout);
+    }, [fetchOptions, debounceTimeout]);
+    return (
+      <Select
+        labelInValue
+        filterOption={false}
+        onSearch={debounceFetcher}
+        notFoundContent={fetching ? <Spin size="small" /> : null}
+        {...props}
+        options={options}
+      />
+    );
+  }
+  async function fetchUserList(username) {
+    console.log('fetching user', username);
+    return fetch('https://randomuser.me/api/?results=5')
+      .then((response) => response.json())
+      .then((body) =>
+        body.results.map((user) => ({
+          label: `${user.name.first} ${user.name.last}`,
+          value: user.login.username,
+        })),
+      );
+  }
+  return (
+    <React.Fragment>
+      <h1>Calculator</h1>
+      <Row justify="center">
+        <Form layout={formLayout} form={form} className="promo-form">
+          <Form.Item label="Choose Product Name">
+            {/* <Select
+              mode="multiple"
+              allowClear
+              style={{width: "100%"}}
+              placeholder="Please select"
+              defaultValue={["a10", "c12"]}
+              onChange={handleChange}
+            >
+              {children}
+            </Select> */}
+            <DebounceSelect
+      mode="multiple"
+      value={value}
+      placeholder="Select users"
+      fetchOptions={fetchUserList}
+      onChange={(newValue) => {
+        setValue(newValue);
+      }}
+      style={{
+        width: '100%',
+      }}
+    />
+          </Form.Item>
+        </Form>
+      </Row>
+    </React.Fragment>
+  );
+};
+
+export default HomePage;
---
+++ src/Components/HomeScreen.js
@@ -0,0 +1,283 @@
+import React, {useState} from "react";
+import {productData} from "../data/index";
+
+const HomeScreen = () => {
+  const [productPrice1, setProductPrice1] = useState("");
+  const [productPrice2, setProductPrice2] = useState("");
+  const [productPrice3, setProductPrice3] = useState("");
+  const [productPrice4, setProductPrice4] = useState("");
+  const [productPrice5, setProductPrice5] = useState("");
+  const [productPrice6, setProductPrice6] = useState("");
+  const [productPrice7, setProductPrice7] = useState("");
+  const [productPrice8, setProductPrice8] = useState("");
+  const [productPrice9, setProductPrice9] = useState("");
+  const [productPrice10, setProductPrice10] = useState("");
+
+  const handleProductValues1 = e => {
+    console.log("e", e.target.value);
+    setProductPrice1(e.target.value);
+  };
+  const handleProductValues2 = e => {
+    console.log("e", e.target.value);
+    setProductPrice2(e.target.value);
+  };
+  const handleProductValues3 = e => {
+    console.log("e", e.target.value);
+    setProductPrice3(e.target.value);
+  };
+  const handleProductValues4 = e => {
+    console.log("e", e.target.value);
+    setProductPrice4(e.target.value);
+  };
+  const handleProductValues5 = e => {
+    console.log("e", e.target.value);
+    setProductPrice5(e.target.value);
+  };
+  const handleProductValues6 = e => {
+    console.log("e", e.target.value);
+    setProductPrice6(e.target.value);
+  };
+  const handleProductValues7 = e => {
+    console.log("e", e.target.value);
+    setProductPrice7(e.target.value);
+  };
+  const handleProductValues8 = e => {
+    console.log("e", e.target.value);
+    setProductPrice8(e.target.value);
+  };
+  const handleProductValues9 = e => {
+    console.log("e", e.target.value);
+    setProductPrice9(e.target.value);
+  };
+  const handleProductValues10 = e => {
+    console.log("e", e.target.value);
+    setProductPrice10(e.target.value);
+  };
+  const calcPrice = (v1, v2, v3, v4, v5, v6, v7, v8, v9, v10) => {
+    console.log("shjs", v1, v1, v3, v4, v5, v6, v7, v8, v9, v10);
+    const sum =
+      Number(v1) +
+      Number(v2) +
+      Number(v3) +
+      Number(v4) +
+      Number(v5) +
+      Number(v6) +
+      Number(v7) +
+      Number(v8) +
+      Number(v9) +
+      Number(v10);
+    console.log("sum", sum);
+    return sum + 10; // included shipping charge 10 rs.
+  };
+
+  const result = e => {
+    e.preventDefault();
+    var prod1 = document.getElementById("myForm").elements["p1"].value;
+    var prod2 = document.getElementById("myForm").elements["p2"].value;
+    var prod3 = document.getElementById("myForm").elements["p3"].value;
+    var prod4 = document.getElementById("myForm").elements["p4"].value;
+    var prod5 = document.getElementById("myForm").elements["p5"].value;
+    var prod6 = document.getElementById("myForm").elements["p6"].value;
+    var prod7 = document.getElementById("myForm").elements["p7"].value;
+    var prod8 = document.getElementById("myForm").elements["p8"].value;
+    var prod9 = document.getElementById("myForm").elements["p9"].value;
+    var prod10 = document.getElementById("myForm").elements["p10"].value;
+    let sum = calcPrice(
+      prod1,
+      prod2,
+      prod3,
+      prod4,
+      prod5,
+      prod6,
+      prod7,
+      prod8,
+      prod9,
+      prod10
+    );
+    document.getElementById(
+      "demo"
+    ).innerHTML = `Total Price Of All Selected Products Is ${sum}
+      `;
+  };
+  return (
+    <div>
+      <h1>Calculator</h1>
+      <form id="myForm" onSubmit={result}>
+        <label>Please Select A Product </label>
+        <select
+          onChange={handleProductValues1}
+          style={{margin: "30px"}}
+          name="p1"
+        >
+          {productData &&
+            productData.map(x => (
+              <option value={x.FinalPrice}>{x.ProductName}</option>
+            ))}
+        </select>
+        {/* {productPrice1} */}
+        <select
+          name="p2"
+          onChange={handleProductValues2}
+          style={{margin: "30px"}}
+        >
+          {productData &&
+            productData.map(x => (
+              <option value={x.FinalPrice}>{x.ProductName}</option>
+            ))}
+        </select>
+        {/* {productPrice2} */}
+        <select
+          name="p3"
+          onChange={handleProductValues3}
+          style={{margin: "30px"}}
+        >
+          {productData &&
+            productData.map(x => (
+              <option value={x.FinalPrice}>{x.ProductName}</option>
+            ))}
+        </select>
+        {/* {productPrice3} */}
+        <select
+          name="p4"
+          onChange={handleProductValues4}
+          style={{margin: "30px"}}
+        >
+          {productData &&
+            productData.map(x => (
+              <option value={x.FinalPrice}>{x.ProductName}</option>
+            ))}
+        </select>
+        {/* {productPrice4} */}
+        <select
+          name="p5"
+          onChange={handleProductValues5}
+          style={{margin: "30px"}}
+        >
+          {productData &&
+            productData.map(x => (
+              <option value={x.FinalPrice}>{x.ProductName}</option>
+            ))}
+        </select>
+        {/* {productPrice5} */}
+        <select
+          name="p6"
+          onChange={handleProductValues6}
+          style={{margin: "30px"}}
+        >
+          {productData &&
+            productData.map(x => (
+              <option value={x.FinalPrice}>{x.ProductName}</option>
+            ))}
+        </select>
+        {/* {productPrice6} */}
+        <select
+          name="p7"
+          onChange={handleProductValues7}
+          style={{margin: "30px"}}
+        >
+          {productData &&
+            productData.map(x => (
+              <option value={x.FinalPrice}>{x.ProductName}</option>
+            ))}
+        </select>
+        {/* {productPrice7} */}
+        <select
+          name="p8"
+          onChange={handleProductValues8}
+          style={{margin: "30px"}}
+        >
+          {productData &&
+            productData.map(x => (
+              <option value={x.FinalPrice}>{x.ProductName}</option>
+            ))}
+        </select>
+        {/* {productPrice8} */}
+        <select
+          name="p9"
+          onChange={handleProductValues9}
+          style={{margin: "30px"}}
+        >
+          {productData &&
+            productData.map(x => (
+              <option value={x.FinalPrice}>{x.ProductName}</option>
+            ))}
+        </select>
+        {/* {productPrice9} */}
+        <select
+          name="p10"
+          onChange={handleProductValues10}
+          style={{margin: "30px"}}
+        >
+          {productData &&
+            productData.map(x => (
+              <option value={x.FinalPrice}>{x.ProductName}</option>
+            ))}
+        </select>
+        {/* {productPrice10} */}
+
+        <input
+          type="submit"
+          style={{backgroundColor: "#285DDB", color: "#fff"}}
+        />
+      </form>
+      <label>Selected Product's Price </label>
+      <span style={{display: "flex", justifyContent: "center"}}>
+        {productPrice1 ? (
+          <b style={{margin: "30px"}}>
+            First Product &nbsp; &nbsp;{productPrice1}
+          </b>
+        ) : null}
+        {productPrice2 ? (
+          <b style={{margin: "30px"}}>
+            Second Product &nbsp; &nbsp;{productPrice2}
+          </b>
+        ) : null}
+        {productPrice3 ? (
+          <b style={{margin: "30px"}}>
+            Third Product &nbsp; &nbsp;{productPrice3}
+          </b>
+        ) : null}
+        {productPrice4 ? (
+          <b style={{margin: "30px"}}>
+            Fourth Product &nbsp; &nbsp;{productPrice4}
+          </b>
+        ) : null}
+        {productPrice5 ? (
+          <b style={{margin: "30px"}}>
+            Fifth Product &nbsp; &nbsp;{productPrice5}
+          </b>
+        ) : null}
+        {productPrice6 ? (
+          <b style={{margin: "30px"}}>
+            Sixth Product &nbsp; &nbsp;{productPrice6}
+          </b>
+        ) : null}
+        {productPrice7 ? (
+          <b style={{margin: "30px"}}>
+            Seventh Product &nbsp; &nbsp;{productPrice7}
+          </b>
+        ) : null}
+        {productPrice8 ? (
+          <b style={{margin: "30px"}}>
+            Eighth Product &nbsp; &nbsp;{productPrice8}
+          </b>
+        ) : null}
+        {productPrice9 ? (
+          <b style={{margin: "30px"}}>
+            Ninth Product &nbsp; &nbsp;{productPrice9}
+          </b>
+        ) : null}
+        {productPrice10 ? (
+          <b style={{margin: "30px"}}>
+            Tenth Product &nbsp; &nbsp;{productPrice10}
+          </b>
+        ) : null}
+      </span>
+      <b>
+        <p id="demo"></p>
+      </b>
+    </div>
+  );
+};
+
+export default HomeScreen;
